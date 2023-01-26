6f46a95bd337980c7111049d77d42caeae3f1184
Vishal Kumar Jha <98863677+itsvishalkjha@users.noreply.github.com>
2023-01-26 10:30:06 +0530

    Update and rename helloworld.py to Livestream.py

--- Livestream.py
+++ Livestream.py
@@ -2,23 +2,23 @@
 
 #to give a comment use a hastag , comment hota kya hai ? comment is a piece of code which the computer does not act upon
 
-#problem 1 -
+# problem 1 -
 #  and friends planned a picnic
 #   gave 5000 rupees ,  and  have 3000 rupees what percent of money  
 # contributed  in the total amount
 
-#here v is amount by  , a is amount by others t is the sum of total amount x and y are used to find 
-# the percentage and also we have used f to find the remaining percentage so that the code looks pretty
+here v is amount by  , a is amount by others t is the sum of total amount x and y are used to find 
+the percentage and also we have used f to find the remaining percentage so that the code looks pretty
 
-# v = 5000
-# a =  3000
-# t = v+a
+v = 5000
+a =  3000
+t = v+a
 
-# x = v/t 
-# y= x*100
-# f = 100-y
-# print("The percentage of money  contributed is :", y)
-# print("The percentage of money others contributed is :", f)
+x = v/t 
+y= x*100
+f = 100-y
+print("The percentage of money  contributed is :", y)
+print("The percentage of money others contributed is :", f)
 
 
 #problem 2 finding the winner of class votin assembly
